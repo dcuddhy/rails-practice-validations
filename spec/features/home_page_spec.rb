2bf9f7aea4c0185e3dd3baa6cdd644a89ad02acf
dcuddhy <dcuddhy@gmail.com>
2014-11-10 11:51:08 -0700

    done-zo

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -7,4 +7,18 @@ feature 'Homepage' do
     expect(page).to have_content("Practice Validations")
   end
 
+scenario 'Person creates value' do
+  visit root_path
+  click_on "People"
+  click_on "New Person"
+  click_on "Create Person"
+
+  expect(page).to have_content("Last name can't be blank")
+end
+
+
+
+
+
+
 end
---
+++ spec/features/person_spec.rb
@@ -0,0 +1,9 @@
+# require 'rails_helper'
+#
+# feature 'Person' do
+#
+#   scenario 'Person creates user'
+#     visit root_path
+#     click_on "People"
+#   end
+# end
